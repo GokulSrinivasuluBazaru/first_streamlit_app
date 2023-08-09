887cf29388b5eb869e847d5abd0074cfff383037
GokulSrinivasuluBazaru <48087002+GokulSrinivasuluBazaru@users.noreply.github.com>
2023-08-09 10:14:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('🥣Breakfast Menu')
-🥣 🥗 🐔 🥑🍞
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spanich & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied  Free-Range Egg')
