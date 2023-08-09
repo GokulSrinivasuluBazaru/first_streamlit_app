9eb986a60da897ee2fc94fc714bfcf73cab84dd0
GokulSrinivasuluBazaru <48087002+GokulSrinivasuluBazaru@users.noreply.github.com>
2023-08-09 10:48:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 import pandas
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Mom's New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spanich & Rocket Smoothie')
 streamlit.text('🐔 Hard-Bolied  Free-Range Egg')
