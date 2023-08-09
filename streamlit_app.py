6201d7e9501a1b29a508c0c230d66d913d88411b
GokulSrinivasuluBazaru <48087002+GokulSrinivasuluBazaru@users.noreply.github.com>
2023-08-09 22:31:49 +0530

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('My Moms New Healthy Dinner')
 
@@ -23,6 +24,5 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
